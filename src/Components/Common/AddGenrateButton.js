8d8138849f9f0bb51cf12595c19138a56572aa51
vikrant-101 <vikrant.r@cisinlabs.com>
2023-05-13 12:42:05 +0530

    Added Add Generate Button And Pdf Button

---
+++ src/Components/Common/AddGenrateButton.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+const AddGenrateButton = ({handleClassesClicks}) => {
+  const { t } = useTranslation();
+  return (
+    <React.Fragment>
+      <button
+        type="button"
+        className="btn btn-primary btn-label rounded-pill"
+        onClick={() => handleClassesClicks('isAdd')}
+      >
+        <i className="ri-add-circle-fill label-icon align-middle rounded-pill fs-16 me-2"></i>{" "}
+        {t('Genrate')}
+      </button>
+    </React.Fragment>
+  )
+}
+
+export default AddGenrateButton;
